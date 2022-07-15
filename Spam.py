a57201bb041ca44de3def1c1d5781f9e9e36f20d
Justsomeguywithadeathnote <109157016+Justsomeguywithadeathnote@users.noreply.github.com>
2022-07-15 19:57:06 +0530

    Create Spam.py

---
+++ Spam.py
@@ -0,0 +1,12 @@
+from pyrogram import Client, filters
+import asyncio
+app=client(session_name=Pyro String, api_id=apiidhere, api_hash=apihashhere)
+ 
+async def main() :
+    async with app:
+        while true:
+            await app.send_message(message.chat.id, " pro ", reply_to_message_id=1046271)
+            await asyncio.sleep(3)
+ 
+ 
+app.run(main())
